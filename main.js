f2f1c1891110c3bc3ac1d85cb21708eca60a826d
Jeancahu <jeancahu@gmail.com>
2022-02-13 18:39:47 -0600

    chg: comment script

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-console.log("Ejecutando en el navegador Animeflv.net redirect block");
+// Removes social networks top bar
 document.getElementsByClassName("FollowUs")[0].remove();
 
-// Borra el script que hace redirects:
+// Delete all scripts nodes
 Array.from(document.getElementsByTagName("SCRIPT")).forEach(
     element => element.remove()
 );
 
-// Borra el redirect
-
+// Set the redirection layer to the very deep behind the content
 (async () => {
     const delay = ms => new Promise(res => setTimeout(res, ms));
+    // Await til the scripts place the redirection layer
     await delay(500);
     Array.from(document.body.getElementsByTagName("DIV")).filter(
         element => element.style.position === "fixed"
     )[0].style.zIndex = -1000;
 })();
 
+// Remove the follow social networks ads
 Array.from(document.body.getElementsByTagName("A")).filter(
     element => element.rel === "nofollow").forEach(element => element.remove());
